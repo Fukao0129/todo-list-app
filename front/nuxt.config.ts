ac5bddbf675f2adf56496603d8ee3c1da3b1cacb
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-04 17:02:40 +0900

    sassをアンインストール

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  ssr: false,
+  devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      apiUrl: "http://localhost/api",
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "ja",
+      },
+    },
+  },
+  css: [
+    "@/assets/style/index.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
+  imports: { dirs: ["@/constants/**"] },
+});
