dead369578ceb56b44379210bb06fabf776e7ae4
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:47:27 +0900

    検索テキストエリアコンポーネントを作成

---
+++ front/app/components/SearchInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const searchText = defineModel<string>("searchText");
+</script>
+
+<template>
+  <div class="search-input__wrapper">
+    <BaseIcon icon="search" class="search-icon" />
+    <input
+      v-model="searchText"
+      type="text"
+      placeholder="キーワードで検索"
+      class="search-input"
+    />
+  </div>
+</template>
+
+<style scoped>
+.search-input__wrapper {
+  position: relative;
+  .search-icon {
+    position: absolute;
+    left: 5px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .search-input {
+    padding-left: 1.8rem;
+  }
+}
+</style>
