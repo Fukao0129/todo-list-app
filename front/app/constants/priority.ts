827c79351af81030f201b6bb3976be5cdf913191
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:30:39 +0900

    優先度を定数化

---
+++ front/app/constants/priority.ts
@@ -0,0 +1,15 @@
+// 優先度
+export const PRIORITY = {
+  LOW: {
+    value: 1,
+    label: "低",
+  },
+  MEDIUM: {
+    value: 2,
+    label: "中",
+  },
+  HIGH: {
+    value: 3,
+    label: "高",
+  },
+};
