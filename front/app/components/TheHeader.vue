165dbea0929ddd9f9b5e4cd9927ee7b6434d5f49
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 09:47:55 +0900

    ヘッダーコンポーネントを作成

---
+++ front/app/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts" />
+
+<template>
+  <header>
+    <NuxtLink to="/" class="logo-link">
+      <img src="/assets/img/logo.svg" alt="TODO"
+    /></NuxtLink>
+  </header>
+</template>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: var(--primary-color);
+  color: white;
+  padding: 1rem;
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid var(--border-color);
+  .logo-link {
+    display: block;
+    width: 7rem;
+    height: auto;
+    transition: all 200ms 0s ease;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+}
+</style>
