c16531da165ace9eb9b0bd457e3b49f2bfd7dd50
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-27 12:44:28 +0900

    inputのコンポーネントを作成

---
+++ front/app/components/Base/BaseInput.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+export type BaseInputProps = {
+  placeholder?: string;
+  errorMessage?: string[];
+  label?: string;
+};
+defineProps<BaseInputProps>();
+
+const text = defineModel<string>("text");
+</script>
+
+<template>
+  <div class="input__wrapper">
+    <BaseText v-if="label" size="small" color="secondary">{{ label }}</BaseText>
+    <input
+      v-model="text"
+      type="text"
+      :placeholder
+      :class="{ 'input--error': errorMessage }"
+    />
+  </div>
+  <BaseText v-if="errorMessage" size="small" color="error" bold>{{
+    errorMessage.join("/ ")
+  }}</BaseText>
+</template>
+
+<style scoped>
+.input__wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 0.1rem;
+}
+input {
+  padding: 0.3rem 0.5rem;
+  border-radius: 5px;
+  border-width: 1px;
+  border-color: var(--border-color);
+  border-style: solid;
+  transition: all 200ms 0s ease;
+  &:hover {
+    background-color: var(--hover-color);
+  }
+  &:focus {
+    border-color: transparent;
+    outline: 2px solid var(--focus-color);
+    background-color: white;
+  }
+}
+
+/** エラー時 */
+.input--error {
+  border-color: var(--error-color);
+}
+</style>
