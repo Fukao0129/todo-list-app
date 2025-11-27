9f056094a7882866dcf561af16fe32541005e569
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-27 12:46:51 +0900

    テキストエリアのコンポーネントを作成

---
+++ front/app/components/Base/BaseTextarea.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+export type BaseTextareaProps = {
+  placeholder?: string;
+};
+defineProps<BaseTextareaProps>();
+
+const text = defineModel<string | null>("text");
+</script>
+
+<template>
+  <textarea v-model="text" :placeholder="placeholder" />
+</template>
+
+<style scoped>
+textarea {
+  padding: 0.3rem 0.5rem;
+  border-radius: 5px;
+  border-width: 1px;
+  border-color: var(--border-color);
+  border-style: solid;
+  transition: all 200ms 0s ease;
+  &:hover {
+    background-color: var(--hover-color);
+  }
+  &:focus {
+    border-color: transparent;
+    outline: 2px solid var(--focus-color);
+    background-color: white;
+  }
+}
+</style>
