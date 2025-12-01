4a30d17501129c1c980efb82336cf1237086c546
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-01 10:00:02 +0900

    認証関係のエンドポイントをweb.phpに追加

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 認証
+Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login');
+Route::post('/logout', 'App\Http\Controllers\LoginController@logout');
+
 Route::get('/', function () {
     return view('welcome');
 });
