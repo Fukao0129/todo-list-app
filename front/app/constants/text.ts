ea8c56d1730dc196d7708dd3e1c2de4641504d42
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-02 18:23:33 +0900

    共通で使うテキストを定数にまとめる

---
+++ front/app/constants/text.ts
@@ -0,0 +1,8 @@
+export const LOADING_TEXT = "読み込み中...";
+export const NO_DATA_TEXT = "データがありません。";
+export const ERROR_TEXT = "エラーが発生しました。";
+export const CANCEL_BUTTON_TEXT = "キャンセル";
+export const DELETE_BUTTON_TEXT = "削除";
+export const UPDATE_BUTTON_TEXT = "更新";
+export const EDIT_BUTTON_TEXT = "編集";
+export const ADD_BUTTON_TEXT = "追加";
