ae2995d838b689d44a037cde23f7873009250c32
DMS深尾 <t.fukao@dm-s.co.jp>
2026-01-03 11:58:59 +0900

    ブラウザ幅が狭いときはサイドバーの表示非表示を切り替えられるようにする

--- front/app/layouts/default.vue
+++ front/app/layouts/default.vue
@@ -7,17 +7,27 @@ withDefaults(
     hasSidebar: true,
   }
 );
+const { isSidebarVisible, hideSidebar } = useSidebar();
 </script>
 
 <template>
   <TheHeader />
 
   <div class="flex min-h-screen">
-    <TheSidebar v-if="hasSidebar" class="hidden md:block" />
+    <TheSidebar
+      v-if="hasSidebar"
+      class="fixed top-header-height w-sidebar-width h-[calc(100vh-3.5rem)] z-20 transition-all md:translate-x-0"
+      :class="{ 'translate-x-[-10rem] ': !isSidebarVisible }"
+    />
     <main
       class="w-full mt-header-height bg-slate-100 p-8 flex flex-col gap-4"
       :class="{ 'md:ml-sidebar-width': hasSidebar }"
     >
+      <div
+        v-if="hasSidebar && isSidebarVisible"
+        class="absolute inset-0 z-10 bg-black/50 flex items-center justify-center md:hidden"
+        @click="hideSidebar()"
+      />
       <slot />
     </main>
   </div>
