5f0520b183c56e94b6dd36e11cdb1662784f25ce
DMS深尾 <t.fukao@dm-s.co.jp>
2026-01-08 18:19:36 +0900

    一部コメントをJSDoc形式に変更

--- front/app/utils/focusOnElement.ts
+++ front/app/utils/focusOnElement.ts
@@ -1,4 +1,6 @@
-/** 指定した要素にフォーカスを当てる */
+/** 指定した要素にフォーカスを当てる
+ *  @param selector フォーカスを当てる対象のセレクター
+ */
 export const focusOnElement = (selector: string) => {
   const element = document.querySelector<HTMLElement>(selector);
 
