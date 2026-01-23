7ac0a236fe4bf1f58ddb8fb578206b2ae45c0e38
DMS深尾 <t.fukao@dm-s.co.jp>
2026-01-23 15:16:12 +0900

    BaseInputのラベルにforを指定

--- front/app/components/Base/BaseInput.vue
+++ front/app/components/Base/BaseInput.vue
@@ -7,16 +7,24 @@ defineProps<{
 defineOptions({ inheritAttrs: false });
 
 const text = defineModel<string>();
+
+const id = useId(); // ラベル用のID
 </script>
 
 <template>
   <div class="flex flex-col gap-1 w-full">
-    <BaseText v-if="label" size="small" color="secondary" tag="label">{{
-      label
-    }}</BaseText>
+    <BaseText
+      v-if="label"
+      size="small"
+      color="secondary"
+      tag="label"
+      :for="id"
+      >{{ label }}</BaseText
+    >
     <input
       v-model="text"
       v-bind="$attrs"
+      :id="id"
       class="p-2 w-full rounded-md border hover:bg-primary-subtle focus:outline-none focus:border-transparent focus:bg-white focus:ring-2 transition-all base-input"
       :class="[
         errorMessage
