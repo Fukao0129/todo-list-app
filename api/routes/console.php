af1922118eba7378caa315ac161bd403fa949499
DMS深尾 <t.fukao@dm-s.co.jp>
2026-04-04 18:26:04 +0900

    リマインド送信のタスクスケジューラとartisanコマンドを設定2 git commit -m リマインド送信のタスクスケジューラとartisanコマンドを設定

--- api/routes/console.php
+++ api/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// リマインドメール送信のタスクスケジューラ
+Schedule::command('todo:send-reminders')->everyMinute();
