331058e890741b2919ab6f705f6c030f2f41bbc0
DMS深尾 <t.fukao@dm-s.co.jp>
2026-04-05 17:59:38 +0900

    タスクスケジューラを5分ごとに変更

--- api/routes/console.php
+++ api/routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 // リマインドメール送信のタスクスケジューラ
-Schedule::command('todo:send-reminders')->everyMinute();
+Schedule::command('todo:send-reminders')->everyFiveMinutes();
