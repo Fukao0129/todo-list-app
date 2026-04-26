9290daddea09c0abf836805c3c79b9335f6b3694
DMS深尾 <t.fukao@dm-s.co.jp>
2026-04-26 15:38:42 +0900

    コントローラークラスにPHPDocを追加

--- api/app/Http/Controllers/StatusController.php
+++ api/app/Http/Controllers/StatusController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\StatusService;
 use App\Http\Requests\StatusRequest;
+use App\Http\Resources\StatusResource;
+use Illuminate\Support\Facades\Auth;
+
 
 class StatusController extends Controller
 {
@@ -14,32 +17,35 @@ class StatusController extends Controller
 
     /**
      * 全件取得
-     *
-     * @param Request $request
+     * @apiResourceCollection App\Http\Resources\StatusResource
+     * @apiResourceModel App\Models\Status
      * @return \Illuminate\Http\JsonResponse
      */
-    function index(Request $request)
+    function index()
     {
-        list($result, $status) = $this->statusService->index($request);
-        return response()->json($result, $status);
+        list($result, $status) = $this->statusService->index();
+        return response()->json(StatusResource::collection($result), $status);
     }
 
     /**
      * 追加
-     *
+     * @apiResource App\Http\Resources\StatusResource
+     * @apiResourceModel App\Models\Status
      * @param StatusRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
     function store(StatusRequest $request)
     {
         $validated = $request->validated();
+        $validated['user_id'] = Auth::id();
         list($result, $status) = $this->statusService->store($validated);
-        return response()->json($result, $status);
+        return response()->json(new StatusResource($result), $status);
     }
 
     /**
      * 更新
-     *
+     * @apiResource App\Http\Resources\StatusResource
+     * @apiResourceModel App\Models\Status
      * @param int $status_id
      * @param StatusRequest $request
      * @return \Illuminate\Http\JsonResponse
@@ -47,13 +53,13 @@ class StatusController extends Controller
     function update($status_id, StatusRequest $request)
     {
         $validated = $request->validated();
+        $validated['user_id'] = Auth::id();
         list($result, $status) = $this->statusService->update($status_id, $validated);
-        return response()->json($result, $status);
+        return response()->json(new StatusResource($result), $status);
     }
 
     /**
      * 削除
-     *
      * @param int $status_id
      * @return \Illuminate\Http\JsonResponse
      */
--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Services\UserService;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
 
 
 class UserController extends Controller
@@ -13,20 +15,26 @@ class UserController extends Controller
     // 依存性注入
     public function __construct(private UserService $userService) {}
 
+    /**
+     * ユーザー情報取得
+     * @apiResource App\Http\Resources\UserResource
+     * @apiResourceModel App\Models\User
+     */
     function me(Request $request)
     {
-        return response()->json($request->user(), 200);
+        return response()->json(new UserResource($request->user()), 200);
     }
 
     /** ユーザー情報更新 
+     * @apiResource App\Http\Resources\UserResource
+     * @apiResourceModel App\Models\User
      * @param UserRequest $request
-     * @param int $user_id
      * @return \Illuminate\Http\JsonResponse
      */
-    function update(UserRequest $request, $user_id)
+    function update(UserRequest $request)
     {
         $validated = $request->validated();
-        list($result, $status) = $this->userService->update($user_id, $validated);
-        return response()->json($result, $status);
+        list($result, $status) = $this->userService->update(Auth::id(), $validated);
+        return response()->json(new UserResource($result), $status);
     }
 }
