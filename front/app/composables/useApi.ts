e83c96be38fad9d7281b33c14b4d3f1598ef610d
Fukao0129 <tomohiro.fukao@icloud.com>
2026-06-17 22:37:30 +0900

    SSRのヘッダーにcookieとrefererのみ転送するようにして、host/accept等の混入で405になるのを防ぐ

--- front/app/composables/useApi.ts
+++ front/app/composables/useApi.ts
@@ -5,7 +5,7 @@ import { defu } from "defu";
 export const useApi = () => {
   const runtimeConfig = useRuntimeConfig();
   const xsrfToken = useCookie("XSRF-TOKEN");
-  const reqHeaders = useRequestHeaders(); // SSR用ヘッダー
+  const reqHeaders = useRequestHeaders(["cookie", "referer"]); // SSR用ヘッダー
 
   /** 共通ヘッダー作成 (毎回最新のCSRFトークンを取得する) */
   const createCommonOptions = () => ({
